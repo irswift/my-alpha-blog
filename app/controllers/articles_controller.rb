c38199314aea6cb2fbda78c42b7345da4613cdb6
Blog App Ruby on Rails <irswift.ir@gmail.com>
2017-01-28 14:56:51 +0330

    Created new and show action and updated edit and index pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+    
+  end
+
   def new
     @article = Article.new
   end
@@ -18,7 +24,19 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was succssfuly updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
 
   private
 
